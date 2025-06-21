7a7090753466747dc7ddfddfd0bffb96ef225a23
Winston White <whitew2015@fau.edu>
2025-06-21 13:11:21 -0400

    issues with several APIs where getting blocked by CORS; will find suitable API to work properly

--- src/App.js
+++ src/App.js
@@ -2,26 +2,47 @@ import './App.css';
 import { useState, useEffect } from 'react';
 
 function App() {
-  
-  const [quote, setQuote] = useState("");
-  const [author, setAuthor] = useState("");
+  const colors = [
+    '#FF6B6B', '#6BCB77', '#4D96FF', '#FFD93D',
+    '#6A4C93', '#00C49A', '#F65A83', '#FF8E00'
+  ];
+
+  const [quote, setQuote] = useState('');
+  const [author, setAuthor] = useState('');
+  const [color, setColor] = useState(colors[0]);
+
+  const fetchQuote = () => {
+    fetch("https://zenquotes.io/api/random") //fetching API 
+      .then(res => res.json())
+      .then(data => {
+        setQuote(data[0].q);
+        setAuthor(data[0].a);
+        const randomColor = colors[Math.floor(Math.random() * colors.length)];
+        setColor(randomColor);
+      })
+      .catch(err => {
+        setQuote("Something went wrong.");
+        setAuthor("Unknown");
+      });
+  };
 
   useEffect(() => {
-    fetch("http://api.quotable.io/random")
-      .then(res => res.json())
-      .then(
-        (quote) => {
-          setQuote(quote.content);
-          setAuthor(quote.author);
-        }
-      )
+    fetchQuote();
   }, []);
 
-  
   return (
-    <div>
-      <h2>{quote}</h2>
-      <small>-{author}-</small>
+    <div className="app" style={{ backgroundColor: color }}>
+      <div className="quote-box">
+        <h2 className="quote">"{quote}"</h2>
+        <small className="author">- {author} -</small>
+        <button
+          className="new-quote-btn"
+          onClick={fetchQuote}
+          style={{ color: color }}
+        >
+          New Quote
+        </button>
+      </div>
     </div>
   );
 }
